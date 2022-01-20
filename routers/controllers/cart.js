2677c12a9104f4b86b7b9e06add1e015af97119f
Ahlamonepiece1989 <Ahlam.Daham.rae.js@tuwaiq.edu.sa>
2022-01-20 15:07:30 +0300

    backend

--- routers/controllers/cart.js
+++ routers/controllers/cart.js
@@ -5,19 +5,11 @@ const addCart = async (req, res) => {
 
 
     const { cartItem } = req.body;
-
-
-    console.log(cartItem);
-
   
   
     try {
 
-        const savedCart = await cartModel.insertMany(cartItem);
-
-
-        console.log(savedCart);
-        
+        const savedCart = await newCart.insertMany(cartItem);
 
         res.status(200).json(savedCart);
   
--- routers/controllers/products.js
+++ routers/controllers/products.js
@@ -1,4 +1,3 @@
-
 const productsModel = require("../../db/models/productsModel");
 
 
